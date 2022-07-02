55d799fd64d1377e0e406eaa0b003057fa01cbaf
Natali <ledesmanatali@gmail.com>
2022-07-02 19:16:41 -0300

    header sin banner y sin bootstrap

--- ProyectoFinal/src/app/app.module.ts
+++ ProyectoFinal/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialMediaComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
