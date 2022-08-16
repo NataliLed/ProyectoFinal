6784f4c8449bf9f5bf166461dadfd3bf67be7122
Natali <ledesmanatali@gmail.com>
2022-08-16 19:22:35 -0300

    backend
    creación de la carpeta backend

---
+++ ProyectoFinal/BackEnd/src/main/java/com/nataliledesma/nel/NelApplication.java
@@ -0,0 +1,13 @@
+package com.nataliledesma.nel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NelApplication.class, args);
+	}
+
+}
---
+++ ProyectoFinal/BackEnd/src/test/java/com/nataliledesma/nel/NelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nataliledesma.nel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
